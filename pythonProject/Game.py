1c6333ece16f7b5a64224d4a64d1707db98b2a37
muravyed123 <65818974+muravyed123@users.noreply.github.com>
2024-06-11 14:28:45 +0300

    Update the first

--- pythonProject/Game.py
+++ pythonProject/Game.py
@@ -1,1 +1,18 @@
 import pygame as pg
+#import tensorflow as tf
+import numpy as np
+pg.init()
+WIDTH = 1200
+HEIGHT = 800
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+r = pg.Rect(50, 50, 100, 200)
+pg.draw.rect(screen, (255, 0, 0), r, 0)
+doing = True
+while doing:
+    pg.display.flip()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            doing = False
+
+pg.quit()
