87f40713ada16a1976b2fbd8e43abeb7b38251ef
prognic <nikerm112358@gmail.com>
2024-06-11 17:34:58 +0300

    FINALLY

--- pythonProject/Game.py
+++ pythonProject/Game.py
@@ -15,7 +15,6 @@ r3 = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2+195, 600, 10)
 pg.draw.rect(screen, (255, 255, 255), r3, 0)
 r4 = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2+395, 600, 10)
 pg.draw.rect(screen, (255, 255, 255), r4, 0)
-<<<<<<< HEAD
 
 class Button():
     def __init__(self, x, y, width, height, onclickFunction=None, onePress=False, index = 0):
@@ -43,43 +42,30 @@ class Button():
                 self.alreadyPressed = False
 
 n = 0
-a = [[325, 125], [525, 125], [725, 125], [325, 325], [525, 325], [725, 325], [325, 525], [525, 525], [725, 525]]
-b = [[475, 275],[675, 275], [875, 275], [475, 475],[675, 475], [875, 475], [475, 675],[675, 675], [875, 675]]
-=======
-n=0
 a=[[325, 125], [525, 125], [725, 125], [325, 325], [525, 325], [725, 325], [325, 525], [525, 525], [725, 525]]
 b=[[475, 275],[675, 275], [875, 275], [475, 475],[675, 475], [875, 475], [475, 675],[675, 675], [875, 675]]
 c=[[325, 275], [525, 275], [725, 275], [325, 475], [525, 475], [725, 475], [325, 675], [525, 675], [725, 675]]
 d=[[475, 125],[675, 125], [875, 125], [475, 325],[675, 325], [875, 325], [475, 525],[675, 525], [875, 525]]
->>>>>>> b829aa1c65340040d77123c40c0842da4a7246a7
 def krestik(n, i):
     if n % 2 == 0:
         pg.draw.line(screen, (255, 255, 255), (a[i][0]-7, a[i][1]), (b[i][0]-7, b[i][1]),7)
         pg.draw.line(screen, (0, 0, 255), (a[i][0], a[i][1]), (b[i][0], b[i][1]), 7)
         pg.draw.line(screen, (255, 0, 0), (a[i][0]+7, a[i][1]), (b[i][0]+7, b[i][1]), 7)
-<<<<<<< HEAD
-Data = [0]*9
-n = 0
-=======
-    if n%2==1:
+    else:
         pg.draw.line(screen, (0, 0, 0), (c[i][0] - 7, c[i][1]), (d[i][0] - 7, d[i][1]), 7)
         pg.draw.line(screen, (255, 255, 0), (c[i][0], c[i][1]), (d[i][0], d[i][1]), 7)
         pg.draw.line(screen, (255, 255, 255), (c[i][0] + 7, c[i][1]), (d[i][0] + 7, d[i][1]), 7)
-for i in range (9):
-    krestik(0, i)
-    krestik(1, i)
-
-
->>>>>>> b829aa1c65340040d77123c40c0842da4a7246a7
 doing = True
+Data = [0]*9
 buttons = []
 def validate(ind):
     global n
-    if Data[ind] == 3 or (Data[ind] != 0 and (Data[ind]-n) % 2 == 0):
+    if Data[ind] == 3 or (Data[ind] != 0 and (Data[ind]-n) % 2 != 0):
         return False
     else:
         krestik(n, ind)
         n += 1
+        Data[ind]+=(2 - n%2)
         return True
 for i in range(9):
     but = Button(300 + i%3*200, 100 + i//3 * 200, 200, 200, validate, False, i)
