e948661d497c465df253627e6ac4c085fc095efc
oleg14052005 <71787731+oleg14052005@users.noreply.github.com>
2024-06-11 16:50:39 +0300

    Update Game.py

--- pythonProject/Game.py
+++ pythonProject/Game.py
@@ -15,6 +15,15 @@ r3 = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2+195, 600, 10)
 pg.draw.rect(screen, (255, 255, 255), r3, 0)
 r4 = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2+395, 600, 10)
 pg.draw.rect(screen, (255, 255, 255), r4, 0)
+n=0
+a=[[325, 325]]
+b=[[475, 475]]
+def krestik(n, i):
+    if n%2==0:
+        pg.draw.line(screen, (255, 255, 255), (a[i][0]-5, a[i][1]), (b[i][0]-5, b[i][1]),5)
+        pg.draw.line(screen, (0, 0, 255), (a[i][0], a[i][1]), (b[i][0], b[i][1]), 5)
+        pg.draw.line(screen, (255, 0, 0), (a[i][0]+5, a[i][1]), (b[i][0]+5, b[i][1]), 5)
+krestik(0, 0)
 doing = True
 while doing:
     pg.display.flip()
