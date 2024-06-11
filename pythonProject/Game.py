27dc26abd85e08b8b2316c602b6991ef33edc06c
oleg14052005 <71787731+oleg14052005@users.noreply.github.com>
2024-06-11 16:10:42 +0300

    g

--- pythonProject/Game.py
+++ pythonProject/Game.py
@@ -5,9 +5,16 @@ pg.init()
 WIDTH = 1200
 HEIGHT = 800
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-
 r = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2, 600, 600)
 pg.draw.rect(screen, (200, 130, 90), r, 0)
+r1 = pg.Rect((WIDTH - 600)//2+195, (HEIGHT - 600)//2, 10, 600)
+pg.draw.rect(screen, (255, 255, 255), r1, 0)
+r2 = pg.Rect((WIDTH - 600)//2+395, (HEIGHT - 600)//2, 10, 600)
+pg.draw.rect(screen, (255, 255, 255), r2, 0)
+r3 = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2+195, 600, 10)
+pg.draw.rect(screen, (255, 255, 255), r3, 0)
+r4 = pg.Rect((WIDTH - 600)//2, (HEIGHT - 600)//2+395, 600, 10)
+pg.draw.rect(screen, (255, 255, 255), r4, 0)
 doing = True
 while doing:
     pg.display.flip()
