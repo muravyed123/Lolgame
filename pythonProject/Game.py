2713b872e6ca5607bc1fdbd03800e79b6d618fa3
muravyed123 <65818974+muravyed123@users.noreply.github.com>
2024-06-17 12:36:06 +0300

    YES, it can WIN !!!!!!

--- pythonProject/Game.py
+++ pythonProject/Game.py
@@ -1,23 +1,28 @@
 import pygame as pg
 import numpy as np
 import pygame.time
-import Proga as pr
+import Proga as Pr
 import random
 
 pg.init()
 WIDTH = 1200
 HEIGHT = 800
-PROGA_PLAIT = True
+PROG_PLAIT = True
+
 clock = pygame.time.Clock()
 Screen = pg.display.set_mode((WIDTH, HEIGHT))
 screen = pg.Surface((WIDTH, HEIGHT))
-screen.fill((0,0,0))
+screen.fill((0, 0, 0))
+
 Alldata11 = []
 Alldata12 = []
 Alldata21 = []
 Alldata22 = []
+n = 0
+
+
 def draw():
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     r = pg.Rect((WIDTH - 600) // 2, (HEIGHT - 600) // 2, 600, 600)
     pg.draw.rect(screen, (200, 100, 90), r, 0)
     r1 = pg.Rect((WIDTH - 600) // 2 + 195, (HEIGHT - 600) // 2, 10, 600)
@@ -28,8 +33,10 @@ def draw():
     pg.draw.rect(screen, (255, 255, 255), r3, 0)
     r4 = pg.Rect((WIDTH - 600) // 2, (HEIGHT - 600) // 2 + 395, 600, 10)
     pg.draw.rect(screen, (255, 255, 255), r4, 0)
+
+
 class Button():
-    def __init__(self, x, y, width, height, onclickFunction=None, onePress=False, index = 0, text = ''):
+    def __init__(self, x, y, width, height, onclickFunction=None, onePress=False, index=0, text=''):
         self.x = x
         self.y = y
         self.width = width
@@ -50,9 +57,10 @@ class Button():
                 self.buttonRect.height / 2 - self.buttonSurf.get_rect().height / 2
             ])
             screen.blit(self.buttonSurface, self.buttonRect)
+
     def process(self):
-        mousePos = pg.mouse.get_pos()
-        if self.buttonRect.collidepoint(mousePos):
+        mouse_pos = pg.mouse.get_pos()
+        if self.buttonRect.collidepoint(mouse_pos):
             if pg.mouse.get_pressed(num_buttons=3)[0] and can_play:
                 if self.onePress:
                     self.onclickFunction(self.index)
@@ -63,28 +71,34 @@ class Button():
                 self.alreadyPressed = False
 
 
-n = 0
-a=[[325, 125], [525, 125], [725, 125], [325, 325], [525, 325], [725, 325], [325, 525], [525, 525], [725, 525]]
-b=[[475, 275],[675, 275], [875, 275], [475, 475],[675, 475], [875, 475], [475, 675],[675, 675], [875, 675]]
-c=[[325, 275], [525, 275], [725, 275], [325, 475], [525, 475], [725, 475], [325, 675], [525, 675], [725, 675]]
-d=[[475, 125],[675, 125], [875, 125], [475, 325],[675, 325], [875, 325], [475, 525],[675, 525], [875, 525]]
+a = [[325, 125], [525, 125], [725, 125], [325, 325], [525, 325], [725, 325], [325, 525], [525, 525], [725, 525]]
+b = [[475, 275], [675, 275], [875, 275], [475, 475], [675, 475], [875, 475], [475, 675], [675, 675], [875, 675]]
+c = [[325, 275], [525, 275], [725, 275], [325, 475], [525, 475], [725, 475], [325, 675], [525, 675], [725, 675]]
+d = [[475, 125], [675, 125], [875, 125], [475, 325], [675, 325], [875, 325], [475, 525], [675, 525], [875, 525]]
+
+
 def krestik(n, i):
     if n % 2 == 0:
-        pg.draw.line(screen, (255, 255, 255), (a[i][0]-7, a[i][1]), (b[i][0]-7, b[i][1]),7)
+        pg.draw.line(screen, (255, 255, 255), (a[i][0]-7, a[i][1]), (b[i][0]-7, b[i][1]), 7)
         pg.draw.line(screen, (0, 0, 255), (a[i][0], a[i][1]), (b[i][0], b[i][1]), 7)
         pg.draw.line(screen, (255, 0, 0), (a[i][0]+7, a[i][1]), (b[i][0]+7, b[i][1]), 7)
     else:
         pg.draw.line(screen, (0, 0, 0), (c[i][0] - 7, c[i][1]), (d[i][0] - 7, d[i][1]), 7)
         pg.draw.line(screen, (255, 255, 0), (c[i][0], c[i][1]), (d[i][0], d[i][1]), 7)
         pg.draw.line(screen, (255, 255, 255), (c[i][0] + 7, c[i][1]), (d[i][0] + 7, d[i][1]), 7)
+
+
 doing = True
 buttons = []
 Data = [0]*9
 last = -1
 moves = []
-can_play = not PROGA_PLAIT
+can_play = not PROG_PLAIT
+
+
 def validate(ind):
     global n, last, can_play
+
     if Data[ind] == 3 or (Data[ind] != 0 and (Data[ind]-n) % 2 != 0) or ind == last:
         return False
     else:
@@ -95,80 +109,111 @@ def validate(ind):
         moves.append(ind)
         if victory():
             can_play = False
-            replay = Button(10, 50, 130,60, clear, False, 0, 'replay?')
-            buttons.append(replay)
+            if not PROG_PLAIT:
+                replay = Button(10, 50, 130, 60, clear, False, 0, 'replay?')
+                buttons.append(replay)
+            if sum(Data) == 0:
+                return True
             if n % 2 == 0:
                 screen.blit(text1, (350, 250))
             if n % 2 == 1:
                 screen.blit(text, (450, 250))
         return True
+
+
 def clear(i):
     global Data, buttons, last, can_play, moves, n
     draw()
     Data = [0]*9
     buttons = []
     last = -1
-    can_play = not PROGA_PLAIT
+    can_play = not PROG_PLAIT
     moves = []
+    if i == 1 and PROG_PLAIT:
+        if n % 2 == 0:
+            screen.blit(text1, (350, 250))
+        if n % 2 == 1:
+            screen.blit(text, (450, 250))
     n = 0
     for i in range(9):
         but = Button(300 + i % 3 * 200, 100 + i // 3 * 200, 200, 200, validate, False, i)
         buttons.append(but)
-all_conc = np.array([[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]])
+
+
 clear(0)
+
+
 def victory():
+    all_conc = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]])
     for i in all_conc:
         if Data[i[0]] + Data[i[1]] + Data[i[2]] == 9:
+            if PROG_PLAIT:
+                restart_game(True)
             return True
+
     return False
-saluty = [pg.image.load("1.png"), pg.image.load("2.png"), pg.image.load("3.png"), pg.image.load("4.png"), pg.image.load("5.png"), pg.image.load("6.png"), pg.image.load("7.png"), pg.image.load("8.png"), pg.image.load("9.png"), pg.image.load("10.png"), pg.image.load("11.png"), pg.image.load("12.png"), pg.image.load("13.png"), pg.image.load("14.png"), pg.image.load("15.png")]
+
+
 salutt = True
 k = 0
 dt = 0
 timer = 0
-ar = [[300,300],[350,350],[400,300],[300,400],[500,290],[500,500],[450,300],[300,300],[320,400],[400,400],[350,370],[380,434],[340,400],[300,430],[400,400],[350,430]]
+ar = [[300, 300], [350, 350], [400, 300], [300, 400], [500, 290], [500, 500], [450, 300], [300, 300],
+      [320, 400], [400, 400], [350, 370], [380, 434], [340, 400], [300, 430], [400, 400], [350, 430]]
 def salut():
     global k, dt
-    Screen.blit(saluty[k//3], ar[dt%16])
-    if k==42:
-        k=0
-        dt+=1
+    img = pg.image.load(str(k//3+1)+'.png')
+    Screen.blit(img, ar[dt % 16])
+    if k == 42:
+        k = 0
+        dt += 1
     else:
-        k+=1
+        k += 1
+
+
 tim = 0
-def restart_game(how, move):
+
+
+def restart_game(how):
     global Alldata11, Alldata12, Alldata21, Alldata22
-    datax1, datay1, datax2, datay2 = pr.make_data(moves, how, move)
-    if len(Alldata11) >= 10:
-        pr.train_model(Alldata11, Alldata12, Alldata21, Alldata22)
+    datax1, datay1, datax2, datay2 = Pr.make_data(moves, how)
+    clear(int(how))
+    if len(Alldata11) >= 2:
+        Pr.train_model(Alldata11, Alldata12, Alldata21, Alldata22)
         Alldata11 = []
         Alldata12 = []
         Alldata21 = []
         Alldata22 = []
     else:
-        for i in range(len(datax1)):
-            Alldata11.append(datax1[i])
-            Alldata12.append(datay1[i])
-        for i in range(len(datax2)):
-            Alldata21.append(datax2[i])
-            Alldata22.append(datay2[i])
-    clear(0)
-    #validate(random.randint(0, 8))
+        for j in range(10):
+            for i in range(len(datax1)):
+                Alldata11.append(datax1[i])
+                Alldata12.append(datay1[i])
+            for i in range(len(datax2)):
+                Alldata21.append(datax2[i])
+                Alldata22.append(datay2[i])
+    # validate(random.randint(0, 8))
+
+
 def real_move():
     global tim
     tim += 1
     if tim == 2:
         tim = 0
-        mov = pr.make_move(Data, last, n)
+        mov = Pr.make_move(Data, last, n)
         print(mov)
         if not validate(mov):
             moves.append(mov)
-            move = 0
-            for i in range(9):
-                if Data[i] == 0:
-                    move = i
-                    break
-            restart_game(False, move)
+            #
+            # move = 0
+            # for i in range(9):
+            #    if Data[i] == 0:
+            #        move = i
+            #        break
+            restart_game(False)
+            tim = -8
+
+
 font = pygame.font.SysFont('couriernew', 40)
 text = font.render(str('Russia winner'), True, (128, 0, 255))
 text1 = font.render(str('Russian Empire winner'), True, (128, 0, 255))
@@ -177,14 +222,14 @@ while doing:
     pg.display.flip()
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            pr.save_model('now_model')
+            Pr.save_model('now_model')
             doing = False
     for bu in buttons:
         bu.process()
 
-    Screen.blit(screen, (0,0))
+    Screen.blit(screen, (0, 0))
     if not can_play:
-        #salut()
+        # salut()
         real_move()
     clock.tick(30)
 pg.quit()
--- pythonProject/Proga.py
+++ pythonProject/Proga.py
@@ -4,82 +4,96 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras import layers
 import numpy as np
 import random
-def make_data(moves, how_played, move):
+
+
+def make_data(moves, how_played):
     pos = [0]*9
     Datatens1 = []
     Data_y1=[]
     Datatens2 = []
     Data_y2 = []
-    win = (len(moves)+int(not how_played))%2
+    win = (len(moves)+int(not how_played)) % 2
     s = len(moves)
     if not how_played:
-        s-=1
+        s -= 1
     for i in range(len(moves)):
-        if i != s:
-            pos[moves[i]] += (2 - i % 2)
         new_data = [0]*10
+        how_to_move = [0] * 9
+
         for j in range(len(pos)):
             new_data[j] = pos[j]/4
-        new_data[-1] = moves[i]/8
+            if (2 - i % 2 == pos[j] or pos[j] == 0) and (i != 0 and j != moves[i-1]):
+                how_to_move[j] = 0.2
+        if i != 0:
+            new_data[-1] = moves[i - 1]/8
+        else:
+            new_data[-1] = 1
         if i % 2 == 0:
             Datatens1.append(new_data)
         else:
             Datatens2.append(new_data)
-        how_to_move =[0]*9
-        if how_played:
-            if (win-i) % 2 == 0:
-                how_to_move[moves[i]] = 1
+
+        if not how_played:
+            if i == len(moves)-1:
+                how_to_move[moves[i]] = 0
             else:
                 how_to_move[moves[i]] = 1
         else:
-            if i == len(moves)-1:
-                how_to_move[move] = 1
-            else:
-                how_to_move[moves[i]] = 1
+            how_to_move[moves[i]] = ((win - i) % 2 + len(moves)/20)/2
         if i % 2 == 0:
             Data_y1.append(how_to_move)
         else:
             Data_y2.append(how_to_move)
-    #print(moves, how_played)
+        #print(pos, how_to_move)
+        pos[moves[i]] += (1 + i % 2)
     return((Datatens1, Data_y1, Datatens2, Data_y2))
+
+
 def save_model(name):
     # Saving the model to disk
     model1.save(name+'1.h5')
     model2.save(name + '2.h5')
-def load_model(name):
-    #Loading the model from disk
-    model1 = tf.keras.models.load_model(name + '1.h5')
-    model2 = tf.keras.models.load_model(name + '2.h5')
-    return (model1, model2)
-def new_model():
 
+
+def load_model(name):
+    # Loading the model from disk
+    model_load1 = tf.keras.models.load_model(name + '1.h5')
+    model_load2 = tf.keras.models.load_model(name + '2.h5')
+    return model_load1, model_load2
+
+
+def new_model():
     # Создание новой последовательной модели
+    opt = 'Adam'
     if True:
-        model1, model2 = load_model('now_model')
-        model1.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
-        model2.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
+        model_1, model_2 = load_model('now_model')
+        model_1.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+        model_2.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
     else:
         inputs = keras.Input(shape=(10,), name="digits")
-        x = layers.Dense(64, activation="linear", name="dense_1")(inputs)
+        x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
         x = layers.Dense(16, activation="linear", name="dense_2")(x)
         outputs = layers.Dense(9, activation="sigmoid", name="predictions")(x)
-        model1 = keras.Model(inputs=inputs, outputs=outputs)
-        model1.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+        model_1 = keras.Model(inputs=inputs, outputs=outputs)
+        model_1.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
         inputs = keras.Input(shape=(10,), name="digits")
         x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
         x = layers.Dense(32, activation="linear", name="dense_2")(x)
         outputs = layers.Dense(9, activation="sigmoid", name="predictions")(x)
-        model2 = keras.Model(inputs=inputs, outputs=outputs)
-        model2.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+        model_2 = keras.Model(inputs=inputs, outputs=outputs)
+        model_2.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
-    return (model1, model2)
+    return model_1, model_2
+
+
 def train_model(Datax1, Datay1, Datax2, Datay2):
+    ep = 20
     X_and = tf.constant(Datax1, dtype=tf.float16)
     Y_and = tf.constant(Datay1, dtype=tf.float16)
     model1.fit(
         X_and,  # Входные данные для обучения
         Y_and,  # Выходные данные для обучения
-        epochs=30,  # Количество итераций, на которое мы хотим обучить модель
+        epochs=ep,  # Количество итераций, на которое мы хотим обучить модель
         verbose=1  # Уровень детализации при выводе в терминале во время обучения
     )
     X_and = tf.constant(Datax2, dtype=tf.float16)
@@ -87,7 +101,7 @@ def train_model(Datax1, Datay1, Datax2, Datay2):
     model2.fit(
         X_and,  # Входные данные для обучения
         Y_and,  # Выходные данные для обучения
-        epochs=30,  # Количество итераций, на которое мы хотим обучить модель
+        epochs=ep,  # Количество итераций, на которое мы хотим обучить модель
         verbose=1  # Уровень детализации при выводе в терминале во время обучения
     )
 
@@ -96,6 +110,8 @@ def make_move(Data, last, number):
     data = np.array([0.0]*10)
     for i in range(len(Data)):
         data[i] = Data[i]/3
+    if last == -1:
+        last = 8
     data[-1] = last/8
     inputTens = tf.constant([data], dtype=tf.float32)
     if number % 2 == 0:
@@ -103,10 +119,13 @@ def make_move(Data, last, number):
     else:
         g = model2.predict(inputTens)[0]
     print(g)
+
     max, index_max = -1, 0
     for i in range(len(g)):
         if g[i] >= max:
             max = g[i]
             index_max = i
     return index_max
+
+
 model1, model2 = new_model()
